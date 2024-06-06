dc1524ab0b405c97d1f7e6532867321ef088b8a0
thegr8nick <nikkuda9@gmail.com>
2024-06-06 10:02:12 +0300

    done graph

--- include/graph.h
+++ include/graph.h
@@ -1,245 +1,213 @@
-
 #pragma once
 #include <iostream>
-#include <algorithm>
-#include <vector>
-#include <queue>
 #include <unordered_map>
+#include <vector>
+#include <utility>
+#include <queue>
+#include <stack>
 #include <functional>
+#include <limits>
 
-using namespace std;
-
-const int INF = 100000000000;
-
-enum class Color {
-	White, 
-	Black,
-	Grey 
-};
-
-template<typename Vertex, typename Distance = double>
-class Graph {
-public:
-	struct Edge {
-		Vertex from, to;
-		Distance distance;
-	};
-private:
-	vector<Vertex> _vertices;
-	unordered_map<Vertex, vector<Edge>> _edges;
-public:
-	bool has_vertex(const Vertex& v) const {
-		return find(_vertices.begin(), _vertices.end(), v) != _vertices.end();
-	}
-
-	bool add_vertex(const Vertex& v) {
-		if (!has_vertex(v)) {
-			_vertices.push_back(v);
-			_edges[v] = {};
+namespace graph {
+	template<class Vertex, class Distance = double>
+	class Graph {
+	public:
+		Graph() {
+			_data = std::unordered_map<Vertex, std::vector<std::pair<Vertex, Distance>>>();
+		}
+		bool has_vertex(const Vertex& v) const {
+			return _data.find(v) != _data.end();
+		}
+		void add_vertex(const Vertex& v) {
+			_data.insert(std::make_pair(v, std::vector<std::pair<Vertex, Distance>>()));
+		}
+		bool remove_vertex(const Vertex& v) {
+			if (!has_vertex(v)) return false;
+			for (size_t i = 0; i < _data.size(); ++i)
+			{
+				if (i == v) continue;
+				int j = 0;
+				auto it = _data[i].begin();
+				while (it != _data[i].end()) {
+					if (it->first == v) {
+						_data[i].erase(it);
+						it = _data[i].begin() + j;
+					}
+					else {
+						++it;
+						++j;
+					}
+				}
+			}
+			_data.erase(v);
 			return true;
 		}
-		return false;
-	}
-
-	bool remove_vertex(const Vertex& v) {
-		auto it = find(_vertices.begin(), _vertices.end(), v);
-		if (it == _vertices.end())
-			return false;
-		_vertices.erase(it);
-		_edges.erase(v);
-		for (auto& pair : _edges) {
-			auto& edges = pair.second;
-			edges.remove_if([v](const Edge& edge) {return edge.to == v; });
-		}
-		return true;
-	}
-
-	vector<Vertex> vertices() const {
-		return _vertices;
-	}
-
-	bool add_edge(const Vertex& from, const Vertex& to, const Distance& d) {
-		_edges[from].push_back({ from, to, d });
-		return true;
-	}
-
-	bool remove_edge(const Vertex& from, const Vertex& to) {
-		auto it = find_if(_edges[from].begin(), _edges[from].end(), [to](const Edge& edge) {return edge.to == to; });
-		if (it == _edges[from].end())
-			return false;
-		_edges[from].erase(it);
-		return true;
-	}
-
-	bool remove_edge(const Edge& e) {
-		auto it = find(_edges[e.from].begin(), _edges[e.from].end(), e);
-		if (it == _edges[e.from].end())
-			return false;
-		_edges[e.from].erase(it);
-		return true;
-	}
-
-	bool has_edge(const Vertex& from, const Vertex& to) {
-		auto it = find_if(_edges[from].begin(), _edges[from].end(), [to](const Edge& edge) {return edge.to == to; });
-		if (it != _edges[from].end())
-			return false;
-		return true;
-	}
-
-	bool has_edge(const Edge& e) {
-		const auto& edges = _edges[e.from];
-		return find(edges.begin(), edges.end(), e) != edges.end();
-	}
-
-	vector<Edge> edges(const Vertex& vertex) {
-		if (!has_vertex(vertex))
-			out_of_range("Don't have vertex.");
-		return _edges[vertex];
-	}
-
-	size_t order() const {
-		return _vertices.size();
-	}
-
-	size_t degree(const Vertex& v) const {
-		if (!has_vertex(v))
-			throw out_of_range("Vertex is not found.");
-		return _edges.at(v).size();
-	}
-
-	vector<Edge> shortest_path(const Vertex& from, const Vertex& to) const {
-		if (!has_vertex(from) || !has_vertex(to)) {
-			return {};
-		}
-		for (const auto& edges_pair : _edges) {
-			for (const auto& edge : edges_pair.second) {
-				if (edge.distance < 0) {
-					throw runtime_error("Graph contains an edge with negative weight.");
+		void print() const {
+			for (auto& v : _data) {
+				std::cout << v.first << ": ";
+				for (auto& e : v.second)
+				{
+					std::cout << e.first << "(" << e.second << ") ";
 				}
+				std::cout << std::endl;
 			}
+			std::cout << std::endl;
 		}
-		unordered_map<Vertex, Distance> distance;
-		unordered_map<Vertex, Vertex> prev;
-		priority_queue<pair<Distance, Vertex>, vector<pair<Distance, Vertex>>, greater<pair<Distance, Vertex>>> pq;
-		vector<Edge> sp;
-		for (const auto& vertex : _vertices) {
-			distance[vertex] = INF;
-			prev[vertex] = Vertex();
-		}
-		distance[from] = 0;
-		pq.push({ 0, from });
-		while (!pq.empty()) {
-			Vertex u = pq.top().second;
-			pq.pop();
-			for (auto& edge : _edges.at(u)) {
-				Vertex v = edge.to;
-				Distance alt = distance[u] + edge.distance;
-				if (alt < distance[v]) {
-					distance[v] = alt;
-					prev[v] = u;
-					pq.push({ alt, v });
-				}
+		std::vector<Vertex> vertices() const {
+			std::vector<Vertex> vertices;
+			for (auto& elem : _data) {
+				vertices.push_back(elem.first);
 			}
+			return vertices;
 		}
-		if (prev.find(to) == prev.end()) {
-			return {};
+		void add_edge(const Vertex& from, const Vertex& to, const Distance& d) {
+			if (!has_vertex(from) || !has_vertex(to)) return;
+			_data[from].push_back(std::make_pair(to, d));
 		}
-		Vertex current = to;
-		while (current != from) {
-			Vertex parent = prev[current];
-			if (parent == Vertex())
-				return {};
-			for (auto& edge : _edges.at(parent)) {
-				if (edge.to == current) {
-					sp.push_back(edge);
-					break;
-				}
-			}
-			current = parent;
-		}
-		reverse(sp.begin(), sp.end());
-		return sp;
-	}
 
-	vector<Vertex> bfs(const Vertex& start_vertex, function<void(const Vertex&)> action) const {
-		queue<Vertex> q;
-		unordered_map<Vertex, Color> color;
-		unordered_map<Vertex, Vertex> parent;
-		unordered_map<Vertex, Distance> distance;
-		vector<Vertex> visited_vertices;
-		for (const auto& vertex : _vertices) {
-			color[vertex] = Color::White;
-			parent[vertex] = Vertex();
-			distance[vertex] = INF;
-		}
-		q.push(start_vertex);
-		color[start_vertex] = Color::Grey;
-		distance[start_vertex] = 0;
-		while (!q.empty()) {
-			Vertex u = q.front();
-			q.pop();
-			visited_vertices.push_back(u);
-			if (action) {
-				action(u);
-			}
-			for (const auto& edge : _edges.at(u)) {
-				Vertex v = edge.to;
-				if (color[v] == Color::White) {
-					color[v] = Color::Grey;
-					distance[v] = distance[u] + 1;
-					parent[v] = u;
-					q.push(v);
-				}
-			}
-			color[u] = Color::Black;
-		}
-		return visited_vertices;
-	}
-
-	void print() const {
-		for (const auto& vertex : _vertices) {
-			cout << "Vertex: " << vertex << endl;
-			cout << "Edges: " << endl;
-			if (_edges.find(vertex) != _edges.end()) {
-				const auto& edges = _edges.at(vertex);
-				if (!edges.empty()) {
-					for (const auto& edge : edges) {
-						cout << "From: " << edge.from << " To: " << edge.to << " Distance: " << edge.distance << endl;
-					}
+		bool remove_edge(const Vertex& from, const Vertex& to) {
+			if (!has_vertex(from) || !has_vertex(to)) return false;
+			auto it = _data[from].begin();
+			int i = 0;
+			while (it != _data[from].end()) {
+				if (it->first == to) {
+					_data[from].erase(it);
+					it = _data[from].begin() + i;
 				}
 				else {
-					cout << "No outgoing edges." << endl;
+					++it;
+					++i;
 				}
+			}
+			return true;
+		}
+		bool remove_edge(const Vertex& from, const Vertex& to, const Distance& d) {
+			if (!has_vertex(from) || !has_vertex(to)) return false;
+			auto it = _data[from].begin();
+			while (it != _data[from].end() && it->first != to && it->second != d) ++it;
+			if (it == _data[from].end()) return false;
+			_data[from].erase(it);
+			return true;
+		}
+		bool has_edge(const Vertex& from, const Vertex& to) const {
+			if (!has_vertex(from) || !has_vertex(to)) return false;
+			auto it = _data.at(from).begin();
+			while (it != _data.at(from).end() && it->first != to) ++it;
+			return it != _data.at(from).end();
+		}
+
+		bool has_edge(const Vertex& from, const Vertex& to, const Distance& d) const {
+			if (!has_vertex(from) || !has_vertex(to)) return false;
+			auto it = _data.at(from).begin();
+			while (it != _data.at(from).end() && !(it->first == to && it->second == d)) ++it;
+			return it != _data.at(from).end();
+		}
+		std::vector<std::pair<Vertex, Distance>> edges(const Vertex& v) const {
+			std::vector<std::pair<Vertex, Distance>> edges;
+			for (auto& e : _data.at(v)) {
+				edges.push_back(e);
+			}
+			return edges;
+		}
+		size_t degree(const Vertex& v) const {
+			return _data.at(v).size();
+		}
+		size_t order() const {
+			return _data.size();
+		}
+		std::vector<Vertex> walk(const Vertex& start_vertex) {
+			std::vector<bool> visited;
+			for (size_t i = 0; i < _data.size(); i++)
+			{
+				visited.push_back(false);
+			}
+			std::vector<Vertex> walk;
+			std::queue<Vertex> queue;
+			queue.push(start_vertex);
+			while (!queue.empty()) {
+				Vertex u = queue.front();
+				walk.push_back(u);
+				queue.pop();
+				for (const auto& v : _data[u]) {
+					if (!visited[v.first]) {
+						visited[v.first] = true;
+						queue.push(v.first);
+					}
+				}
+			}
+			return walk;
+		}
+		std::vector<std::pair<Vertex, Distance>> shortest_path(const Vertex& from, const Vertex& to) {
+			std::unordered_map<Vertex, std::pair<Vertex, Distance>> dist_pred;
+			std::vector<std::pair<Vertex, Distance>> path;
+			std::vector<Vertex> vertices = this->vertices();
+			std::stack<int> stack;
+			for (auto& e : vertices) {
+				dist_pred[e] = std::make_pair(-1, 1e9);
+			}
+			dist_pred[from] = std::make_pair(-1, 0);
+			stack.push(from);
+			while (!stack.empty()) {
+				Vertex u = stack.top();
+				stack.pop();
+				for (auto& v : _data.at(u)) {
+					if (relax(u, v.first, dist_pred)) stack.push(v.first);
+				}
+			}
+			Vertex finish = to;
+			std::pair<Vertex, Distance> pred = dist_pred.at(finish);
+			if (pred.second == 1e9) {
+				path.insert(path.begin(), std::make_pair(finish, pred.second));
 			}
 			else {
-				cout << "No outgoing edges." << endl;
+				while (finish != from) {
+					path.insert(path.begin(), std::make_pair(finish, pred.second));
+					finish = pred.first;
+					pred = dist_pred.at(finish);
+				}
 			}
-			cout << "" << endl;
+			path.insert(path.begin(), std::make_pair(from, 0));
+			return path;
 		}
-	}
+		Vertex find_warehouse() {
+			std::vector<Vertex> vertices = this->vertices();
+			Vertex optimal_vertex;
+			Distance min_avg_distance = std::numeric_limits<Distance>::infinity();
+			for (const Vertex& v : vertices) {
+				Distance total_distance = 0;
+				int reachable_count = 0;
 
-	Vertex find_warehouse() const {
-		Vertex warehouse;
-		Distance min_max_distance = INF;
-		for (const auto& from : _vertices) {
-			Distance max_distance = 0;
-			for (const auto& to : _vertices) {
-				if (from != to) {
-					vector<Edge> path = shortest_path(from, to);
-					Distance d = 0;
-					for (const auto& edge : path) {
-						d += edge.distance;
+				for (const Vertex& other : vertices) {
+					if (v == other) continue;
+					std::vector<std::pair<Vertex, Distance>> path = shortest_path(v, other);
+					Distance distance = path.back().second;
+					if (distance < std::numeric_limits<Distance>::infinity()) {
+						total_distance += distance;
+						reachable_count++;
 					}
-					if (d > max_distance) {
-						max_distance = d;
+				}
+				if (reachable_count > 0) {
+					Distance avg_distance = total_distance / reachable_count;
+					if (avg_distance < min_avg_distance) {
+						min_avg_distance = avg_distance;
+						optimal_vertex = v;
 					}
 				}
 			}
-			if (max_distance < min_max_distance) {
-				min_max_distance = max_distance;
-				warehouse = from;
-			}
+			return optimal_vertex;
 		}
-		return warehouse;
-	}
-};
+	private:
+		bool relax(const Vertex& u, const Vertex& v, std::unordered_map<Vertex,
+			std::pair<Vertex, Distance>>&dist_pred) {
+			auto from_u = _data.at(u).begin();
+			while (from_u->first != v) ++from_u;
+			if (dist_pred.at(v).second > dist_pred.at(u).second + from_u->second) {
+				dist_pred.at(v).second = dist_pred.at(u).second + from_u->second;
+				dist_pred.at(v).first = u;
+				return true;
+			}
+			return false;
+		}
+		std::unordered_map<Vertex, std::vector<std::pair<Vertex, Distance>>> _data;
+	};
+}
--- main.cpp
+++ main.cpp
@@ -1,22 +1,22 @@
-#include "Graph.h"
+#include "graph.h"
+
+using namespace graph;
+using namespace std;
 
 int main() {
-    Graph<int, double> g;
+    Graph<int> g;
 
     for (int i = 1; i <= 6; ++i) {
         g.add_vertex(i);
     }
 
-    g.add_edge(1, 2, 7);
-    g.add_edge(1, 3, 9);
-    g.add_edge(1, 6, 14);
-    g.add_edge(2, 3, 10);
-    g.add_edge(2, 4, 15);
-    g.add_edge(3, 4, 11);
-    g.add_edge(3, 6, 2);
-    g.add_edge(4, 5, 6);
-    g.add_edge(5, 6, 9);
-
+    g.add_edge(6, 1, 7);
+    g.add_edge(6, 2, 8);
+    g.add_edge(6, 3, 9);
+    g.add_edge(6, 4, 10);
+    g.add_edge(6, 5, 11);
+    
+     
     int best_location = g.find_warehouse();
 
     cout << best_location << endl;
